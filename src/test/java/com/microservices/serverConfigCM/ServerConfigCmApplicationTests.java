59c910fdeb92d0615140030147f1138f8dae3203
Sebastian Boron <sebastian.boron8@gmail.com>
2023-05-28 20:47:59 +0200

    Initial server configuration

---
+++ src/test/java/com/microservices/serverConfigCM/ServerConfigCmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.serverConfigCM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerConfigCmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
